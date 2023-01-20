3814747e323639464140abcdb21499e68005115c
fabianosantos2323 <fabianojunior77@gmail.com>
2023-01-19 22:21:41 -0300

    fix: jquery error

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,6 @@ $('.best_choice_card').hover(
     }, function () {
         let src = $(this).find(".img").attr("src").replace('_white.svg', '')
         $(this).find(".img").attr("src", `${src}.svg`)
-    })
+    }
+)
+
