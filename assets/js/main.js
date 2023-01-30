ea1fff5c316cb89480a6f4c6b74bdd5939e91a36
Alef1412 <102564597+Alef1412@users.noreply.github.com>
2023-01-30 00:57:34 -0300

    fix menu

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+﻿window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  if (window.scrollY > 100) {
+    $("#navigation").addClass("scroll")
+  } else {
+    $("#navigation").removeClass("scroll")
+  }
+}
+
+function showBackTopButtonOnScroll() {
+  if (window.scrollY > 600) {
+    $('#backToTopButton').show(200)
+  } else {
+    $('#backToTopButton').hide(200)
+  }
+}
