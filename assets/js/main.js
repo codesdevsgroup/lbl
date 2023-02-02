c5d9d9bf53bc3da65ca4d871a04edceb4c776fbd
Alef1412 <102564597+Alef1412@users.noreply.github.com>
2023-02-01 22:26:49 -0300

    alter function scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,34 @@
-﻿window.addEventListener('scroll', onScroll)
+﻿/* window.addEventListener('scroll', onScroll)
 
 onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackTopButtonOnScroll()
 }
-
-function showNavOnScroll() {
+ */
+/* function showNavOnScroll() {
   if (window.scrollY > 100) {
     $("#navigation").addClass("scroll")
   } else {
     $("#navigation").removeClass("scroll")
   }
-}
+} */
 
-function showBackTopButtonOnScroll() {
+/* function showBackTopButtonOnScroll() {
   if (window.scrollY > 600) {
     $('#backToTopButton').show(200)
   } else {
     $('#backToTopButton').hide(200)
   }
-}
+} */
+
+const desiredElement = document.getElementById('navigation'); // elemento alvo
+const pixelsAmount = '80'; // Quantidade de pixels a contar do TOP até definir a cor
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > pixelsAmount) {
+    	desiredElement.classList.add('scrol'); // adiciona classe "changeColor"
+    } else {
+      desiredElement.classList.remove('scrol'); // remove classe "changeColor"
+    }
+});
